d3ad4e494989b1544c11491f3ed202b5b457de75
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-02-25 15:39:58 +0000

    add data model

---
+++ app/src/main/java/com/example/costaapp/model/Group.kt
@@ -0,0 +1,18 @@
+package com.example.costaapp.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Group {
+    @SerializedName("type")
+    @Expose
+    var type: String? = null
+
+    @SerializedName("name")
+    @Expose
+    var name: String? = null
+
+    @SerializedName("items")
+    @Expose
+    var items: List<Item>? = null
+}
---
+++ app/src/main/java/com/example/costaapp/model/VenueLocation.kt
@@ -0,0 +1,5 @@
+package com.example.costaapp.model
+
+class VenueLocation {
+
+}
