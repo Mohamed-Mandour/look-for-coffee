d32000125fde1eb1c94130c3a0207e5ad87c3da7
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-02-26 14:22:17 +0000

    create VenueDatabase

---
+++ app/src/main/java/com/example/costaapp/database/VenueDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.costaapp.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.costaapp.model.Venue
+
+@TypeConverters(VenueConverter::class)
+@Database(entities = [Venue::class], version = 1)
+abstract class VenueDatabase : RoomDatabase() {
+
+    abstract fun venueDao(): VenueDao
+
+    companion object {
+        private val lock = Any()
+        private const val DB_NAME = "VenueDatabase"
+        private var INSTANCE: VenueDatabase? = null
+
+        fun getInstance(application: Application): VenueDatabase {
+            synchronized(lock) {
+                if (INSTANCE == null) {
+                    INSTANCE =
+                        Room.databaseBuilder(application, VenueDatabase::class.java, DB_NAME)
+                            .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
