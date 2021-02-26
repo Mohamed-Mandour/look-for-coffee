2d52a77bd2b84341a6d92fd17133270aa5e8f96f
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-02-26 15:08:07 +0000

    add getSavedVenue

--- app/src/main/java/com/example/costaapp/repository/VenueRepository.kt
+++ app/src/main/java/com/example/costaapp/repository/VenueRepository.kt
@@ -3,12 +3,15 @@ package com.example.costaapp.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.costaapp.model.Item
+import com.example.costaapp.model.Venue
 
 interface VenueRepository {
 
-   fun getVenue(): MutableLiveData<List<Item>>
+    fun getVenue(): MutableLiveData<List<Item>>
+
+    fun getSavedVenue(): LiveData<List<Venue>>
 
     fun getVenueLocation(): LiveData<String>
 
-   fun searchVenue(query: String): LiveData<List<Item>>
+    fun searchVenue(query: String): LiveData<List<Item>>
 }
