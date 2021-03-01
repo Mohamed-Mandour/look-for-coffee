2fadde99c4e23e5f7aefe45a7d8bcfb7a3a61659
Mohamed Mandour <mohamedmaqndour@opensignal.com>
2021-03-01 01:03:39 +0000

    add Koin dependency injection tool

--- app/src/main/java/com/example/costaapp/App.kt
+++ app/src/main/java/com/example/costaapp/App.kt
@@ -1,13 +1,16 @@
 package com.example.costaapp
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.example.costaapp.database.VenueDatabase
 import com.example.costaapp.repository.LocationRepository
 import com.example.costaapp.repository.LocationRepositoryImpl
+import com.example.costaapp.utils.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 lateinit var db: VenueDatabase
 lateinit var locationRepository: LocationRepository
-class App : Application() {
+class App : MultiDexApplication() {
 
     companion object {
         lateinit var INSTANCE: App
@@ -23,5 +26,11 @@ class App : Application() {
         locationRepository = LocationRepositoryImpl
         locationRepository.location
         db = VenueDatabase.getInstance(this)
+        startKoin {
+            // declare Android context
+            androidContext(this@App)
+            // declare used modules
+            modules(appModule)
+        }
     }
 }
